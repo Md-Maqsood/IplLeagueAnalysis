ccb4844aa4fe26f662f2ab767faff91d6915e586
maqsood <maqsoodhamd37@gmail.com>
2020-10-28 21:02:32 +0530

    [Maqsood].add: initial commit

---
+++ src/main/java/com/bridgelabs/iplleagueanalysis/IplAnalyser.java
@@ -0,0 +1,12 @@
+package com.bridgelabs.iplleagueanalysis;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class IplAnalyser {
+	private static final Logger logger = LogManager.getLogger(IplAnalyser.class);
+
+	public static void main(String[] args) {
+		logger.info("Welcome to IPL League Analyser program");
+	}
+}
